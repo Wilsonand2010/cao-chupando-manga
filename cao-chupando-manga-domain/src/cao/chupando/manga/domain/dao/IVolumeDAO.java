9a4b020a894178999f1a26356d0965747b668b56
Wilsonand2010 <Wilsonand2015@hotmail.com>
2019-07-31 20:54:48 -0400

    Adiciona DAO

---
+++ cao-chupando-manga-domain/src/cao/chupando/manga/domain/dao/IVolumeDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cao.chupando.manga.domain.dao;
+
+import cao.chupando.manga.domain.entidades.Volume;
+import java.util.List;
+
+/**
+ *
+ * @author aluno
+ */
+public interface IVolumeDAO {
+    
+    public void inserir(Volume ent);
+    public void atualizar(Volume ent);
+    public void remover(int id);
+    public List<Volume> consultar();
+}
